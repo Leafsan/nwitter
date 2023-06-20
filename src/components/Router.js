8c1185028ffee0dedaab75b38eb0bf2728e11f07
Wookyung Choi <cwk0906@naver.com>
2023-06-20 17:32:47 +0900

    Nweet component added and CRUD feature added

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,14 +9,14 @@ import Auth from "routes/Auth";
 import Navigation from "./Navigation";
 import Profile from "../routes/Profile";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
       {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<Home userObj={userObj} />} />
             <Route exact path="/profile" element={<Profile />} />
           </>
         ) : (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,66 @@
-const Home = () => <span>Home</span>;
+import { useEffect, useState } from "react";
+import { addDoc, onSnapshot, collection } from "firebase/firestore";
+import { dbService } from "../fbase";
+import Nweet from "../components/Nweet";
+
+const Home = ({ userObj }) => {
+  console.log(userObj);
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(collection(dbService, "nweets"), (snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
+  }, []);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(dbService, "nweets"), {
+      text: nweet,
+      createdAt: Date.now(),
+      createdId: userObj.uid,
+    });
+    setNweet("");
+  };
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {nweets
+          .sort((a, b) => b.createdAt - a.createdAt)
+          .map((nweet) => (
+            <Nweet
+              key={nweet.id}
+              nweetObj={nweet}
+              isOwner={nweet.createdId === userObj.uid}
+            />
+          ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
