c8c56e0255a5914314d63f6df8be429788531a6f
Wookyung Choi <cwk0906@naver.com>
2023-06-22 09:41:42 +0900

    router changed

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,50 +1,45 @@
-import {
-  HashRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
 import Navigation from "./Navigation";
 import Profile from "../routes/Profile";
 import MyNweet from "../routes/MyNweet";
 
-const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
-  return (
-    <Router>
-      {isLoggedIn && <Navigation userObj={userObj} />}
-      <Routes>
-        {isLoggedIn ? (
-          <div
-            style={{
-              maxWidth: 890,
-              width: "100%",
-              margin: "0 auto",
-              marginTop: 80,
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Route exact path="/" element={<Home userObj={userObj} />} />
-            <Route
-              exact
-              path="/profile"
-              element={<Profile refreshUser={refreshUser} userObj={userObj} />}
-            />
-            <Route
-              exact
-              path="/myNweet"
-              element={<MyNweet userObj={userObj} />}
-            />
-          </div>
-        ) : (
-          <Route exact path="/" element={<Auth />} />
-        )}
-        <Route path="*" element={<Navigate replace to="/" />} />
-      </Routes>
-    </Router>
-  );
+const AppRouter = ({isLoggedIn, userObj, refreshUser}) => {
+    return (
+        <BrowserRouter basename={}>
+            {isLoggedIn && <Navigation userObj={userObj}/>}
+            <Routes>
+                {isLoggedIn ? (
+                    <div
+                        style={{
+                            maxWidth: 890,
+                            width: "100%",
+                            margin: "0 auto",
+                            marginTop: 80,
+                            display: "flex",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Route exact path="/" element={<Home userObj={userObj}/>}/>
+                        <Route
+                            exact
+                            path="/profile"
+                            element={<Profile refreshUser={refreshUser} userObj={userObj}/>}
+                        />
+                        <Route
+                            exact
+                            path="/myNweet"
+                            element={<MyNweet userObj={userObj}/>}
+                        />
+                    </div>
+                ) : (
+                    <Route exact path="/" element={<Auth/>}/>
+                )}
+                <Route path="*" element={<Navigate replace to="/"/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default AppRouter;
